7cf7f08e9183a1076baf8ab8ab43782214fdc877
ERSİN ÇÖL <ersincol235@gmail.com>
2022-05-03 12:13:59 +0300

    work linearSearchAlgortihm

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] array={2,4,0,1,9};
+        for(int i:array){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number to search: ");
+        int x=scanner.nextInt();
+        if(linearSearch(array,x)==-1){
+            System.out.println("Element not found...");
+        }
+        else{
+            System.out.println("Element found at index: "+linearSearch(array,x));
+        }
+    }
+
+    public static int linearSearch(int[] array,int x){
+        for(int i=0;i<array.length;i++){
+            if(array[i]==x){
+                return i;
+            }
+        }
+        return -1;
     }
 }
